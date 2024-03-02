ab88529d66c580b72db2f7691210d4478959b5a4
jessiezhang199 <jiz199@ucsd.edu>
2024-03-01 22:39:25 -0800

    push

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -8,10 +8,11 @@
   import Graph from "./Global.svelte";
 
 </script>
-<HomePage />
+
 <Router>
 <Route path="/yes" component={YesPage} />
 <Route path="/no" component={NoPage} />
 <!-- <Route path="/home" component={HomePage} /> -->
 </Router>
+<Graph/>
 
