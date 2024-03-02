9836eae8d9dd2a3c8b386de4fd33040a317f792d
Your Name <vica031106@gmail.com>
2024-03-02 01:38:13 -0800

    add data

--- src/components/Yespage.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-  
-</script>
-<main>
-    <h1>MBTI Elements Explanation</h1>
-    <p>Detailed explanation of each MBTI element (e, i, t, f, p, j, s, n) for those who answered "Yes".</p>
-  </main>
