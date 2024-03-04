abb435f2503ce1ad5a1f62dd97eb677cc959f54e
jessiezhang199 <jiz199@ucsd.edu>
2024-03-03 21:21:46 -0800

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,10 +4,10 @@
   </script>
   
   <nav>
-    <a href="#" on:click|preventDefault={() => goto(`${base}/`)}>Home</a>
-    <a href="#" on:click|preventDefault={() => goto(`${base}/16picture`)}>MBTI Types</a>
-    <a href="#" on:click|preventDefault={() => goto(`${base}/vis`)}>Country Distribution</a>
-    <a href="#" on:click|preventDefault={() => goto(`${base}/quiz`)}>Fun Quiz</a>
+    <a href="/" on:click|preventDefault={() => goto(`${base}/`)}>Home</a>
+    <a href="/" on:click|preventDefault={() => goto(`${base}/16picture`)}>MBTI Types</a>
+    <a href="/" on:click|preventDefault={() => goto(`${base}/vis`)}>Country Distribution</a>
+    <a href="/" on:click|preventDefault={() => goto(`${base}/quiz`)}>Fun Quiz</a>
   </nav>
   
   <slot />
