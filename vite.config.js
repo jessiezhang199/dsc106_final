f9cc570e23de6fc79795c489713320fa93efd59b
Your Name <vica031106@gmail.com>
2024-03-03 19:18:59 -0800

    change

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { resolve } from 'path';
 
 export default {
-  base: process.env.NODE_ENV === 'production' ? '/dsc106_final/' : '/',,
+  base: process.env.NODE_ENV === 'production' ? '/dsc106_final/' : '/',
   plugins: [sveltekit()],
   resolve: {
     alias: {
