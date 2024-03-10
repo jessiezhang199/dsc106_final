99d34cefd29a15dad975ec527baad41f78f353a0
zhanzhan986 <117331602+zhanzhan986@users.noreply.github.com>
2024-03-09 21:50:53 -0800

    beautify quiz page

--- src/routes/quiz/+page.svelte
+++ src/routes/quiz/+page.svelte
@@ -1,10 +1,15 @@
+<footer>
+  <p>Data source: <a href="https://www.16personalities.com/personality-types" target="_blank">https://www.16personalities.com/personality-types</a></p>
+</footer>
+
 <script>
   import { writable } from 'svelte/store';
   import { base } from '$app/paths';
+  import { fade } from 'svelte/transition';
 
   const categoryImages = {
     "Barbie": ["ISFJ-Gloria.jpg", "ISFP-Allen.jpg", "ESFJ-Barbie.jpg", "ESFP-Ken.jpg"],
-    "DC Comics": ["ENFJ-Wonder_woman.jpg", "ENFP-Harley_Quinn.jpg", "ENTP-The_Joker.jpg", "ESTP-Aquaman.jpg", "ESTP-Cat_Woman.jpg", "INFP-The_Flash.jpg", "INTJ-Batman.jpg", "ISFJ-Superman.jpg", "ISTJ-Cyborg.jpg"],
+    "DC Comics": ["ENFJ-Wonder_Woman.jpg", "ENFP-Harley_Quinn.jpg", "ENTP-The_Joker.jpg", "ESTP-Aquaman.jpg", "ESTP-Cat_Woman.jpg", "INFP-The_Flash.jpg", "INTJ-Batman.jpg", "ISFJ-Superman.jpg", "ISTJ-Cyborg.jpg"],
     "Harry Potter": ["ENTP-Fred_Weasley&George_Weasley.jpg", "ESFP-Ron_Weasley.jpg", "ESTJ-Hermione_Granger.jpg", "INFP-Luna_Lovegood.jpg", "ISFJ-Ginny_Weasley.jpg", "ISFP-Harry_Potter.jpg"],
     "The Avengers": ["ENFP-Spider_Man.jpg", "ENTP-Iron_Man.jpg", "ESTP-God_of_Thunder.jpg", "ESTP-Rocket_Raccoon.jpg", "INFJ-Vision.jpg", "INFP-Scarlet_Witch.jpg", "INTJ-Doctor_Strange.jpg", "ISFJ-Captain_America.jpg", "ISFJ-Groot.jpg", "ISFP-Hulk.jpg", "ISTP-Black_Widow.jpg"]
   };
@@ -20,8 +25,42 @@
   let showResults = false;
   let resultsMessage = '';
   let feedbackImage = '';
+  let characterName = '';
+
 
   const allMBTITypes = ["INTJ", "ENTP", "INFP", "ENTJ", "ENFJ", "ISTJ", "ISFJ", "ESTP", "ESFP", "ENFP", "INTP", "ISTP", "ISFP", "ESTJ", "ESFJ", "INFJ"];
+  const imageToCharacterMap = {
+  "ISFJ-Gloria.jpg": "Gloria",
+  "ISFP-Allen.jpg": "Allen",
+  "ESFJ-Barbie.jpg": "Barbie",
+  "ESFP-Ken.jpg": "Ken",
+  "ENFJ-Wonder_Woman.jpg": "Wonder Woman",
+  "ENFP-Harley_Quinn.jpg": "Harley Quinn",
+  "ENTP-The_Joker.jpg": "The Joker",
+  "ESTP-Aquaman.jpg": "Aquaman",
+  "ESTP-Cat_Woman.jpg": "Cat Woman",
+  "INFP-The_Flash.jpg": "The Flash",
+  "INTJ-Batman.jpg": "Batman",
+  "ISFJ-Superman.jpg": "Superman",
+  "ISTJ-Cyborg.jpg": "Cyborg",
+  "ENTP-Fred_Weasley&George_Weasley.jpg": "Fred & George Weasley",
+  "ESFP-Ron_Weasley.jpg": "Ron Weasley",
+  "ESTJ-Hermione_Granger.jpg": "Hermione Granger",
+  "INFP-Luna_Lovegood.jpg": "Luna Lovegood",
+  "ISFJ-Ginny_Weasley.jpg": "Ginny Weasley",
+  "ISFP-Harry_Potter.jpg": "Harry Potter",
+  "ENFP-Spider_Man.jpg": "Spider-Man",
+  "ENTP-Iron_Man.jpg": "Iron Man",
+  "ESTP-God_of_Thunder.jpg": "Thor",
+  "ESTP-Rocket_Raccoon.jpg": "Rocket Raccoon",
+  "INFJ-Vision.jpg": "Vision",
+  "INFP-Scarlet_Witch.jpg": "Scarlet Witch",
+  "INTJ-Doctor_Strange.jpg": "Doctor Strange",
+  "ISFJ-Captain_America.jpg": "Captain America",
+  "ISFJ-Groot.jpg": "Groot",
+  "ISFP-Hulk.jpg": "Hulk",
+  "ISTP-Black_Widow.jpg": "Black Widow"
+};
 
   function startQuiz() {
     imagesForQuiz = selectRandomImages(categoryImages[selectedCategory]);
@@ -32,9 +71,14 @@
     loadQuestion();
   }
 
+  function getCharacterNameFromImage(imageFileName) {
+  return imageToCharacterMap[imageFileName] || "Unknown Character";
+  }
+
   function loadQuestion() {
     let imageFileName = imagesForQuiz[currentQuestionIndex];
     let correctAnswer = imageFileName.split('-')[0];
+    characterName = getCharacterNameFromImage(imageFileName); // This will get the character name from the dictionary
     let incorrectAnswers = allMBTITypes.filter(mbti => mbti !== correctAnswer);
     let randomIncorrectAnswer = incorrectAnswers[Math.floor(Math.random() * incorrectAnswers.length)];
     currentChoices = shuffleArray([correctAnswer, randomIncorrectAnswer]);
@@ -115,8 +159,9 @@
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
-  min-height: 100vh;
+  justify-content: flex-start; /* Changed from center to flex-start */
+  min-height: 90vh; /* Reduced from 100vh to pull content up */
+  margin-top: 40px; /* Example of pulling the content up by reducing the top margin */
 }
 
 .quiz-container {
@@ -151,6 +196,7 @@ button {
   border: none;
   border-radius: 6px;
   transition: background-color 0.3s ease;
+  font-size: 18px;
 }
 
 button:hover {
@@ -161,6 +207,10 @@ h2 {
   text-align: center;
   font-size: 24px;
   margin-bottom: 20px;
+}
+.quiz-container h2 {
+  margin-top: 0px; /* Negative value pulls the content up */
+  /* Other styling properties */
 }
 
 p {
@@ -181,6 +231,28 @@ p {
     max-height: 400px; /* Adjust this value as needed */
     margin-bottom: 20px;
   }
+  footer {
+    position: absolute;
+    bottom: -20px;
+    width: 100%;
+    text-align: center;
+    padding: 10px;
+    background-color: #f3f3f3; /* Or any color that fits your design */
+  }
+
+  footer p {
+    margin: 0;
+    font-size: 14px;
+  }
+
+  footer a {
+    color: #0645AD; /* Or any color that fits your design */
+    text-decoration: none;
+  }
+
+  footer a:hover {
+    text-decoration: underline;
+  }
   
 </style>
 
@@ -200,6 +272,7 @@ p {
 {:else if quizStarted}
   <div class="centered-container">
     <div class="quiz-container">
+      <h2>{characterName}</h2> <!-- Character name displayed here -->
       <img class="quiz-image" src={`${base}/mbti_images/quiz_data/${selectedCategory}/${imagesForQuiz[currentQuestionIndex]}`} alt="Character image" />
       <div class="choices-container">
         {#each currentChoices as choice}
@@ -207,7 +280,7 @@ p {
         {/each}
       </div>
       {#if showFeedback}
-        <p>{feedbackMessage}</p>
+      <p in:fade={{ duration: 500 }}>{feedbackMessage}</p>
       {/if}
     </div>
   </div>
@@ -218,7 +291,7 @@ p {
     {/if}
     <p>{resultsMessage}</p>
     <div class="restart-button-container">
-      <button on:click={restartQuiz}>Give another shot and see what other cool characters we have!</button>
+      <button on:click={restartQuiz}>Thank you for the journey with Capybara! <br> Give another shot and see what other cool characters we have!</button>
     </div>
   </div>
 {/if}
